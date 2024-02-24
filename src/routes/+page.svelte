5e914b4cabe5565c0a11fd3b281405761891fafc
Gabriele Midulla <56894431+gabrielemidulla@users.noreply.github.com>
2024-02-24 16:36:21 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="w-screen h-screen flex flex-col items-center justify-center text-gray-600 gap-4">
+    <h1 class="font-mono text-7xl glow font-black p-2 select-none">glow</h1>
+    <h1 class="max-w-lg text-center">A text-to-image open-source tool, write something down here and look at the magic!</h1>
+    <div class="w-full max-w-3xl flex flex-row">
+        <input type="text" placeholder="Spongebob surfing a lava sea" class="shadow-xl p-5 w-full rounded-full outline-none">
+        <button class="w-44 bg-indigo-600 text-white rounded-full">Magic trick!</button>
+    </div>
+</div>
